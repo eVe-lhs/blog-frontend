0bd9f08fbc859c90210db6edb73eb232ce2d9567
Lin Htet Swe <linhtetswe2012@gmail.com>
2023-07-28 19:16:27 +0630

    choose interest

--- src/pages/ChooseInterests.js
+++ src/pages/ChooseInterests.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header";
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function ChooseInterest() {
   return (
@@ -32,27 +32,76 @@ export default function ChooseInterest() {
     </motion.div>
   );
 }
+
+const topics = [
+  {
+    id: 1,
+    name: "Science",
+    active: "false",
+  },
+  {
+    id: 2,
+    name: "Sports",
+    active: "false",
+  },
+  {
+    id: 3,
+    name: "Music",
+    active: "false",
+  },
+  {
+    id: 4,
+    name: "Movies",
+    active: "false",
+  },
+  {
+    id: 5,
+    name: "Computer Science",
+    active: "false",
+  },
+  {
+    id: 6,
+    name: "AI",
+    active: "false",
+  },
+  {
+    id: 7,
+    name: "Agriculture",
+    active: "false",
+  },
+  {
+    id: 8,
+    name: "Medical Science",
+    active: "false",
+  },
+  {
+    id: 9,
+    name: "Celebrity",
+    active: "false",
+  },
+  {
+    id: 10,
+    name: "Politics",
+    active: "false",
+  },
+  {
+    id: 11,
+    name: "Animals",
+    active: "false",
+  },
+  {
+    id: 12,
+    name: "Natural Disasters",
+    active: "false",
+  },
+];
+
 const ChooseInterests = () => {
-    const topics = []
-    const [selected,setSelected] = useState([])
-    console.log(selected)
-    for (let i = 0; i < 16; i++){
-         topics.push(
-           <motion.div
-                 initial={{ opacity: 0, y: 10 }}
-                 animate={{ opacity: 1, y: 0 }}
-                 onClick={() => setSelected}
-                 transition={{
-                     type: "tween",
-                     stiffness: 200,
-                     damping: 20,
-                     delay: (i + 1) * 0.15
-                 }}
-                 className={` rounded-lg py-2 fill-none text-center border transition-colors duration-300 border-red-300 hover:bg-red-500 hover:text-white hover:cursor-pointer`}
-           >
-             Technology
-           </motion.div>
-         );
+    const [selected, setSelected] = useState([])
+    const toggleClass = (topicId) => {
+        if (!selected.includes(topicId)) setSelected([...selected, topicId])
+        else setSelected(current => current.filter(id => {return id !== topicId}))
+        console.log(selected)
     }
       return (
         <div className="mt-5 ">
@@ -61,13 +110,34 @@ const ChooseInterests = () => {
             transition={{ duration: 0.3 }}
             className="grid grid-flow-row gap-4 grid-cols-4 justify-center"
           >
-            {topics}
+                  {topics.map((topic) => (
+              <motion.div
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                onClick={() => toggleClass(topic.id)}
+                transition={{
+                  type: "tween",
+                  stiffness: 200,
+                  damping: 20,
+                  delay: topic.id * 0.15,
+                }}
+                key={topic.id}
+                id={topic.id}
+                className={`rounded-lg ${
+                    selected.includes(topic.id)
+                    ? "border-red-500 border-2 text-red-500"
+                    : "border-red-300 border"
+                } py-2 fill-none text-center transition-colors duration-300 hover:bg-red-500 hover:text-white hover:cursor-pointer grid items-center justify-center `}
+              >
+                {topic.name}
+              </motion.div>
+            ))}
           </motion.div>
           <motion.button
-            initial={{ opacity: 0}}
-            animate={{ opacity: 1}}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
             transition={{
-              delay: 15 * 0.15 + 0.2,
+              delay: topics.length * 0.15 + 0.2,
             }}
             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 mt-10"
           >
