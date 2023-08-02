5539cfe2fcc6c476433fdd626b82ba7862951f93
LHS <linhtetswe2012@gmail.com>
2023-08-02 10:31:47 +0630

    minor change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -124,9 +124,14 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
                   Lin Htet Swe
                 </p>
               </div>
-              <div class="inline-flex items-center text-base font-semibold text-primary dark:text-white">
-                <button className="hover:border-b-2 border-primary transition duration-100">
+              <div class="inline-flex items-center text-base font-semibold text-white">
+                <button className="p-2 rounded-md  bg-primary hover:bg-secondary transition duration-100">
                   View Profile
+                </button>
+              </div>
+              <div class="inline-flex items-center text-base font-semibold text-white">
+                <button className="text-xs p-2 rounded-md hover:bg-red-600 bg-red-500  cursor-pointer transition duration-100">
+                  Log Out
                 </button>
               </div>
             </div>
@@ -256,17 +261,43 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
             >
               {colorTheme === "dark" ? (
                 <button
-                  className="mx-auto w-10  border-2 px-2 py-2 rounded shadow-lg border-gray-800 bg-gray-900 hover:bg-gray-700 text-gray-900 hover:text-gray-700 text-center transition-all duration-200"
+                  className="mx-auto border-2 px-1 py-1.5 rounded-lg shadow-lg  bg-gray-900 hover:bg-gray-700 text-white  text-center transition-all duration-200"
                   onClick={changeThemeHandler}
                 >
-                  ''
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                    />
+                  </svg>
                 </button>
               ) : (
                 <button
-                  className="mx-auto w-10  border-2 px-2 py-2 rounded border-gray-100 bg-white text-white hover:bg-gray-100 shadow-lg text-center"
+                  className="mx-auto border-2 px-1 py-1.5 rounded-lg bg-white text-gray-900 hover:bg-gray-100 shadow-lg text-center"
                   onClick={changeThemeHandler}
                 >
-                  ''
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-8 h-8"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                    />
+                  </svg>
                 </button>
               )}
             </motion.div>
