f220af61ff7763b1c8d84e6431ca83212f9131b9
Lin Htet Swe <linhtetswe2012@gmail.com>
2023-08-21 20:36:53 +0630

    minor change on sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -105,7 +105,7 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
       <motion.div
         onClick={toggleSidebar}
         className={`md:hidden block fixed px-2 py-0.5 rounded-t-xl border-x border-t drop-shadow-2xl shadow-2xl bg-gray-50 dark:bg-gray-900 justify-center transition-bottom translate-y-1 ease-linear duration-200 z-20 ${
-          open ? "bottom-1/4" : "bottom-20"
+          open ? "bottom-32" : "bottom-20"
         } left-1/2 -translate-x-1/2`}
       >
         <motion.svg
@@ -116,21 +116,21 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
           stroke="currentColor"
           className={`w-6 h-6 origin-center transition duration-200 ease-linear ${
             open ? "rotate-180" : ""
-            } `}
-          animate={{ y:-3, rotate: open? 180:0 }}
+          } `}
+          animate={{ y: -2, rotate: open ? 180 : 0 }}
           transition={{
             y: {
               repeat: Infinity,
               type: "spring",
-              mass:0.4,
+              mass: 0.4,
               repeatType: "reverse",
               duration: 0.4,
-              repeatDelay: 1
+              repeatDelay: 1,
             },
             rotate: {
               duration: 0.2,
-              type:'tween'
-            }
+              type: "tween",
+            },
           }}
         >
           <path
@@ -144,7 +144,7 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
         href="/"
         className={`lg:w-64 md:w-1/4 w-full md:pt-5 px-4 fixed z-20 ease-linear bottom-0 left-0 md:inset-y-0 mr-5 transform md:h-screen md:overflow-y-auto 
         ${
-          !open ? "h-20" : "h-1/4"
+          !open ? "h-20" : "h-32"
         }   md:translate-y-0  md:transition-none transition-height md:border-t-0 md:rounded-none rounded-t-3xl duration-200 md:bg-gray-200 bg-gray-100 font-body text-gray-800 dark:md:bg-gray-800 dark:bg-gray-900 dark:text-white border-t shadow-2xl drop-shadow-2xl md:shadow-xl md:border-r border-r-gray-300`}
         variants={variant}
         initial="hidden"
@@ -168,154 +168,149 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
                   Lin Htet Swe
                 </p>
               </div>
-              <div class="inline-flex items-center text-base font-semibold text-white">
-                <button className="text-xs p-2 rounded-md hover:bg-red-600 bg-red-500  cursor-pointer transition duration-100">
-                  Log Out
-                </button>
-              </div>
             </div>
           </div>
         </motion.div>
 
         <nav
           nav
-          className="md:block flex flex-row md:gap-5 justify-evenly md:mx-auto md:w-36 font-thin md:space-y-0 md:mt-6 md:text-lg text-xs"
+          className="md:block flex flex-col md:mx-auto md:w-36 font-thin md:space-y-0 md:mt-6 md:text-lg text-xs"
         >
-          <motion.div className="mx-auto" variants={navChildVariant}>
-            <div
-             
-              className="navigation w-full"
-              onClick={() => navigate("/home")}
-            >
-              <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-center items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                  />
-                </svg>
+          <div className="md:block flex flex-row md:gap-5 justify-evenly">
+            <motion.div className="mx-auto" variants={navChildVariant}>
+              <div
+                className="navigation w-full"
+                onClick={() => navigate("/home")}
+              >
+                <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                    />
+                  </svg>
 
-                <div className="md:block hidden">Home</div>
+                  <div className="md:block hidden">Home</div>
+                </div>
               </div>
-            </div>
-          </motion.div>
-          <motion.div className="mx-auto" variants={navChildVariant}>
-            <div
-              
-              className="navigation w-full"
-              onClick={(e) => navigate("Profile/1")}
-            >
-              <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-start items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                  />
-                </svg>
+            </motion.div>
+            <motion.div className="mx-auto" variants={navChildVariant}>
+              <div
+                className="navigation w-full"
+                onClick={(e) => navigate("Profile/1")}
+              >
+                <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-start items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                    />
+                  </svg>
 
-                <div className="md:block hidden">Profile</div>
+                  <div className="md:block hidden">Profile</div>
+                </div>
               </div>
-            </div>
-          </motion.div>
+            </motion.div>
 
-          <motion.div
-            className="md:hidden block mx-auto"
-            variants={navChildVariant}
-          >
-            <div
-              href="Bookmarks"
-              className="navigation w-full"
-              onClick={() => setShowModal(true)}
+            <motion.div
+              className="md:hidden block mx-auto"
+              variants={navChildVariant}
             >
-              <div className="flex md:flex-row p-1 flex-col rounded-full bg-primary drop-shadow-2xl md:gap-5 gap-1 md:justify-normal justify-center items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-50"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 4.5v15m7.5-7.5h-15"
-                  />
-                </svg>
+              <div
+                href="Bookmarks"
+                className="navigation w-full"
+                onClick={() => setShowModal(true)}
+              >
+                <div className="flex md:flex-row p-1 flex-col rounded-full bg-primary drop-shadow-2xl md:gap-5 gap-1 md:justify-normal justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-50"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                  </svg>
+                </div>
               </div>
-            </div>
-          </motion.div>
+            </motion.div>
 
-          <motion.div className="mx-auto" variants={navChildVariant}>
-            <div
-              className="navigation w-full"
-              onClick={() => navigate("Bookmarks")}
-            >
-              <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-center items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
-                  />
-                </svg>
+            <motion.div className="mx-auto" variants={navChildVariant}>
+              <div
+                className="navigation w-full"
+                onClick={() => navigate("Bookmarks")}
+              >
+                <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                    />
+                  </svg>
 
-                <div className="md:block hidden">Bookmarks</div>
+                  <div className="md:block hidden">Bookmarks</div>
+                </div>
               </div>
-            </div>
-          </motion.div>
-          <motion.div className="mx-auto" variants={navChildVariant}>
-            <div
-              className="navigation w-full"
-              onClick={() => navigate("Gallery")}
-            >
-              <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-center items-center">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-                  />
-                </svg>
-                <div className="md:block hidden">Gallery</div>
+            </motion.div>
+            <motion.div className="mx-auto" variants={navChildVariant}>
+              <div
+                className="navigation w-full"
+                onClick={() => navigate("Gallery")}
+              >
+                <div className="flex md:flex-row p-1 flex-col md:gap-5 gap-1 md:justify-normal justify-center items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="md:w-6 md:h-6 w-8 h-8 stroke-current text-gray-800 dark:text-gray-50 hover:scale-110 cursor-pointer"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                    />
+                  </svg>
+                  <div className="md:block hidden">Gallery</div>
+                </div>
               </div>
-            </div>
-          </motion.div>
+            </motion.div>
+          </div>
           <motion.div
             className="flex flex-row gap-2"
             variants={navChildVariant}
           >
             <div
-              className="md:block hidden shadow-lg mt-5 md:my-auto text-md text-white font-medium rounded bg-primary hover:scale-105 transition duration-200 mx-auto cursor-pointer text-center px-2 md:px-1.5 py-1.5"
+              className="md:block hidden shadow-lg mt-5 md:my-auto text-md text-white font-medium rounded bg-primary hover:bg-emerald-500 transition duration-200 mx-auto cursor-pointer text-center px-2 md:px-1.5 py-1.5"
               onClick={() => setShowModal(true)}
             >
               New Post
@@ -367,6 +362,11 @@ const SideBar = ({ showModal, setShowModal, colorTheme, setTheme }) => {
               )}
             </motion.div>
           </motion.div>
+          <div class="md:inline-flex block items-center text-base font-semibold text-white w-full">
+            <button className="text-base w-full py-2 rounded-md hover:bg-red-600 bg-red-500  cursor-pointer transition duration-100">
+              Log Out
+            </button>
+          </div>
         </nav>
       </motion.aside>
     </>
